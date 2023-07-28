4a9c2c231890bdc85c640ea4f9497dc175406007
Karan Vishwakarma <karanvish.56@gmail.comgit config --global user.name Karan>
2023-07-28 11:25:00 +0530

    Add  page made

--- newsite/views.py
+++ newsite/views.py
@@ -1,6 +1,18 @@
-from django.shortcuts import HttpResponse,render
+from django.shortcuts import HttpResponse, render
+
 
 def index(request):
-    return HttpResponse( "Hello")
+    return HttpResponse("Hello")
+
+
 def home(request):
-    return render(request,"hello.html")
+    a = 0
+    b = 0
+    result = 0
+    if request.GET:
+        # print("Posted")
+        a=int(request.GET["n1"])
+        b=int(request.GET["n2"])
+        result=a+b
+
+    return render(request, "hello.html", {"a": a, "b": b, "result": result})
