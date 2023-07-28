34ab5fca4a0b6d60c3612031b2a3135e8f221c85
Karan Vishwakarma <karanvish.56@gmail.comgit config --global user.name Karan>
2023-07-28 19:51:25 +0530

    list calculator

--- newsite/urls.py
+++ newsite/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index),
     path('home/', views.home),
-    path('s',views.sub)
+    path('s',views.sub),
+    path('s1',views.cal),
+    path('s2',views.newcal),
+    path('s3',views.list),
 ]
--- newsite/views.py
+++ newsite/views.py
@@ -1,3 +1,4 @@
+from audioop import add
 from django.shortcuts import HttpResponse, render
 
 
@@ -29,3 +30,66 @@ def sub(request):
     return render(request,"subtract.html",{"n":n , "m":m,"result":result})
     
 
+
+def cal(request):
+    a=0
+    b=0
+    result=0
+
+    if request.GET:
+        a=int(request.GET["n1"])
+        b=int(request.GET["n2"])
+        cmd=request.GET["option"]
+        if cmd=="add":
+
+            result=a+b
+        elif cmd=="sub":
+            result=a-b
+        
+        elif cmd=="mul":
+            result=a*b
+
+        elif cmd=="div":
+            result=a/b        
+    
+    return render(request,"calculator1.html",{"a":a,"b":b,"result":result})
+
+
+def newcal(request):
+    a=0
+    b=0
+    result=0
+    if request.GET:
+        a=int(request.GET["n1"])
+        b=int(request.GET["n2"])
+        cmd=request.GET["option"]
+
+        if cmd=="add":
+            result=a+b
+        elif cmd=="sub":
+            result=a-b
+        elif cmd=="mul":
+            result=a*b
+        elif cmd=="div":
+            result=a/b
+    return render(request,"newcal.html",{"a":a,"b":b,"result":result})
+
+def list(request):
+    a=0
+    b=0
+    result=0
+
+    if request.GET:
+        a=int(request.GET["n1"])
+        b=int(request.GET["n2"])
+
+        cmd=request.GET("operation")
+
+        if cmd=="add":
+            result=a+b
+
+        elif cmd=="sub":
+            result=a+b
+
+        
+    return render(request,"listcalculator.html",{"a":a,"b":b,"result":result})
