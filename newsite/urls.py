d9e7315331f22ae1f23489e3ea7fdb92dd2497a2
Karan Vishwakarma <karanvish.56@gmail.comgit config --global user.name Karan>
2023-07-28 12:01:34 +0530

    subtraction

--- newsite/urls.py
+++ newsite/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index),
     path('home/', views.home),
+    path('s',views.sub)
 ]
--- newsite/views.py
+++ newsite/views.py
@@ -11,8 +11,21 @@ def home(request):
     result = 0
     if request.GET:
         # print("Posted")
-        a=int(request.GET["n1"])
-        b=int(request.GET["n2"])
-        result=a+b
+        a = int(request.GET["n1"])
+        b = int(request.GET["n2"])
+        result = a + b
 
     return render(request, "hello.html", {"a": a, "b": b, "result": result})
+def sub(request):
+    n=0
+    m=0
+    result=0
+
+    if request.GET:
+        # print("Posted")
+        n=int(request.GET["n1"])
+        m=int(request.GET["n2"])
+        result=n-m
+    return render(request,"subtract.html",{"n":n , "m":m,"result":result})
+    
+
